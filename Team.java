7044066b653ec558f0630727a112b38e96efef7e
alexkilin <49560127+alexkilin@users.noreply.github.com>
2019-06-19 13:52:58 +0300

    готовое ДЗ к уроку 1

--- Team.java
+++ Team.java
@@ -1,60 +1,76 @@
 package lesson1.marathon;
 
-import lesson1.marathon.competitors.Cat;
 import lesson1.marathon.competitors.Competitor;
-import lesson1.marathon.competitors.Dog;
-import lesson1.marathon.competitors.Human;
-import lesson1.marathon.obstacles.Cross;
 import lesson1.marathon.obstacles.Obstacle;
-import lesson1.marathon.obstacles.Wall;
-import lesson1.marathon.obstacles.Water;
 
 
 
 public class Team {
-    public static String name;
 
-    public static void main(String[] args) {
-        Competitor[] DreamTeam = {new Human("Bob"),
-                new Cat("Vaska"), new Dog("Tuzik"), new Human("Mike")};
+    public String name;
+    Competitor[] DreamTeam;
 
+    public Team (String name, Competitor...DreamTeam) {
+        this.name=name;
+        this.DreamTeam=DreamTeam;
+    }
 
+    public  void go (Obstacle[] obstacles) {
         for (Competitor c:DreamTeam) {
-            System.out.println(c.name+" run: "+c.run+" jump: " +DreamTeam[i].jump+ " swim: " +DreamTeam[i].swim);
-        };
-        }
-
-
-
-       /* for (int i = 0; i<3; i++) {
-            System.out.println(DreamTeam[i].name+" run: "+DreamTeam[i].run+" jump: " +DreamTeam[i].jump+ " swim: " +DreamTeam[i].swim);
-        }
-        for (int i = 0; i<3; i++) {
-            if (DreamTeam[i].isOnDistance) {
-                System.out.println(DreamTeam[i].name);
-            }
-        }
-        ListofTeam ();
-        ListofBestMembers();*/
-
-    }
-
-    /*public static void ListofTeam (){
-
-        for (int i = 0; i<3; i++) {
-           System.out.println(DreamTeam[i].name+" run: "+DreamTeam[i].run+" jump: " +DreamTeam[i].jump+ " swim: " +DreamTeam[i].swim);
-        }
-
-    }
-    public static void ListofBestMembers (){
-        for (int i = 0; i<3; i++) {
-            if (DreamTeam[i].isOnDistance) {
-                System.out.println(DreamTeam[i].name);
+            for (Obstacle o:obstacles) {
+                o.doIt(c);
+                if (!c.isOnDistance()){
+                    break;
+                }
             }
         }
 
-    }*/
+    }
+   public Competitor [] getDreamTeam() {
+      return  DreamTeam;
+   }
+//    public void ListofTeam (){
+//        for (Competitor c:DreamTeam) {
+//            if(c.isOnDistance()) {
+//                c.info();
+//            }
+//        }
+//    }
 
 
+
+
+    public void showResults() {
+        for (Competitor c:DreamTeam) {
+            if(c.isOnDistance()) {
+                c.info();
+            }
         }
-        ЪЪЪ
+    }
+
+//    public void showAbilities () {
+  //      for (Competitor c:DreamTeam) {
+   //         System.out.println(c.maxRunDisctance());
+   //         }
+   //     }
+        public void showAbilities () {
+          for (Competitor c:DreamTeam) {
+             c.inforun();
+             }
+         }
+
+
+
+
+   //    public void ListofBestMembers (){ //второй вариант метода
+//        for (int i = 0; i<4; i++) {
+//            if (DreamTeam[i].isOnDistance()) {
+//                DreamTeam[i].info();
+//            }
+//        }
+//
+//    }
+
+
+}
+
